d36fdcc60ccb4dfb7027069907b9285106191dfc
Harsh <harshk0056@gmail.com>
2023-08-06 15:11:54 +0530

    vercel

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,8 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
-const cors = require('cors');
 
 const app = express();
-app.use(cors);
 
 dotenv.config();
 connectDB();
--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -45,7 +45,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "https://talkzone-backend.vercel.app/api/user/login",
+        "/api/user/login",
         { email, password },
         config
       );
