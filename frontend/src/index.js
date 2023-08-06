300f8e65a29ee6496cdb8af44508eb195769b2e5
Harsh <harshk0056@gmail.com>
2023-08-07 00:50:30 +0530

    render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ import { ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme/theme.js";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'http://127.0.0.1:5000';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
